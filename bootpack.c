364917d3cca42fe173a19c59bc4f93f843cc060c
zhangda <zhangda@zhangdadeMacBook-Pro.local>
2024-04-06 23:06:50 +0800

    palette

--- bootpack.c
+++ bootpack.c
@@ -1,12 +1,21 @@
 void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
 void write_mem8(int addr, int data);
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+
 
 void _DAOS_Main(void) {
   int i;
   char *p;
+  init_palette();
+  //0xa0000是左上角的像素点
   for(i = 0xa0000; i <=0xaffff; i++){
     p = (char *)i;
-    *p = i & 0x0f;
+    *p = i & 0x0f; //选择调色板上的一个颜色，这里确保在0~15之间
     // or use the library:
     // write_mem8(i, i & 0x0f);
   }
@@ -14,3 +23,40 @@ void _DAOS_Main(void) {
     io_hlt();
   }
 }
+
+void init_palette(void){
+  static unsigned char table_rgb[16*3] = {
+    0x00, 0x00, 0x00,
+    0x66, 0x00, 0x00,
+    0xcc, 0x00, 0x00,
+    0xff, 0x00, 0x00,
+    0x00, 0x66, 0x00,
+    0x00, 0xcc, 0x00,
+    0x00, 0xff, 0x00,
+    0x00, 0x00, 0x66,
+    0x00, 0x00, 0xcc,
+    0x00, 0x00, 0xff,
+    0xcc, 0xcc, 0x00,
+    0xff, 0xff, 0x00,
+    0xcc, 0x00, 0xcc,
+    0xff, 0x00, 0xff,
+    0x00, 0xff, 0xff,
+    0xff, 0xff, 0xff,
+  };
+  set_palette(0, 15, table_rgb);
+}
+
+void set_palette(int start, int end, unsigned char *rgb){
+  int i, eflags;
+  eflags = io_load_eflags(); //记录中断许可标志的值
+  io_cli();//中断许可标志设置为0
+  io_out8(0x3c8, start); //将想要设定的调色板号码写入0x3c8，然后将rgb写入0x3c9
+  for(i = start; i <= end; i++){
+    io_out8(0x3c9, rgb[0]/4);
+    io_out8(0x3c9, rgb[1]/4);
+    io_out8(0x3c9, rgb[2]/4);
+    rgb += 3;
+  }
+  io_store_eflags(eflags);
+}
+
--- func.asm
+++ func.asm
@@ -3,6 +3,10 @@
   [BITS 32]
 
   GLOBAL io_hlt, write_mem8     ; 程序中包含函数名
+  GLOBAL io_cli, io_sti
+  GLOBAL io_in8, io_in16, io_in32
+  GLOBAL io_out8, io_out16, io_out32
+  GLOBAL io_load_eflags, io_store_eflags
 
   [SECTION .text]
 io_hlt:              ; void io_hlt(void);
@@ -14,3 +18,51 @@ write_mem8:             ; void write_mem8(int addr, int data);
    MOV     AL, [ESP+8]   ; data
    MOV     [ECX], AL
    RET
+
+io_cli:
+  CLI
+  RET
+io_sti:
+  STI
+  RET
+io_in8: ;int io_in8(int port)
+  MOV EDX, [ESP + 4]
+  MOV EAX, 0
+  IN AL, DX
+  RET
+io_in16: ;int io_in16(int port)
+  MOV EDX, [ESP + 4]
+  MOV EAX, 0
+  IN AX, DX
+  RET
+io_in32: ;int io_in32(int port)
+  MOV EDX, [ESP + 4]
+  MOV EAX, 0
+  IN EAX, DX
+  RET
+io_out8: ; void io_out8(int port, int data)
+  MOV EDX, [ESP + 4]
+  MOV AL, [ESP + 8]
+  OUT DX, AL
+  RET
+io_out16: ; void io_out16(int port, int data)
+  MOV EDX, [ESP + 4]
+  MOV EAX, [ESP + 8]
+  OUT DX, AX
+  RET
+io_out32: ; void io_out32(int port, int data)
+  MOV EDX, [ESP + 4]
+  MOV EAX, [ESP + 8]
+  OUT DX, EAX
+  RET
+io_load_eflags:
+  PUSHFD ; Push Flags Double-word
+  POP EAX
+  RET
+io_store_eflags:
+  MOV EAX, [ESP + 4]
+  PUSH EAX
+  POPFD
+  RET
+
+
